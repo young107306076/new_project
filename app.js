bbe88e265531a8b14789878f3915ea8d3af5d3f5
Young <44830681+young107306076@users.noreply.github.com>
2022-04-13 20:31:14 +0800

    Test 3 API

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ var mysql = require('mysql')
 
 //Database Setting up
 let conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'todoapp'
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'todoapp'
 });
 
 // 建立連線後不論是否成功都會呼叫
 conn.connect(function(err){
-    if(err) throw err;
-    console.log('connect success!');
+	if(err) throw err;
+	console.log('connect success!');
 });
 
 //這是首頁
 app.get('/', function (req, res) {
-   res.send('Hello World');
+	res.send('Hello World');
 })
 
 //設計API
@@ -27,33 +27,52 @@ app.get('/', function (req, res) {
 //Product Details API
 //Product Create API
 //這個比較看不懂.... (但應該就是商品的數量，可能也要根據不同種類區分)
-app.get('/Product/List',function(req, res) {
+app.get('/Product/List/:id',function(req, res) { //這是其中一種取得parameter的方法
+	//取得page_id
+	var page_id = req.params.id;
 
-    //取得產品的各項資訊
-    conn.query('SELECT * FROM `user`', function(err, result, fields){
-        if(err) throw err;
-        console.log(result);
-    });
+	//取得產品的各項資訊
+	// conn.query('SELECT * FROM `user`', function(err, result, fields){
+	// 	if(err) throw err;
+	// 	console.log(result);
+	// });
+
+	//test
+	console.log("取得Paging"+page_id);
 })
 
 //他要的是keywork看有沒有符合的title (Product Title)
-app.get('/Product/Search',function(req, res){
+app.get('/Product/Search',function(req, res){//這則是另外一種，用body-parser的方式
+	//取得查詢的keyword
+	var keyword = req.body.keyword;
 
+	//取得符合該關鍵字的產品資訊
+	// conn.query('SELECT * FROM `user`', function(err, result, fields){
+	// 	if(err) throw err;
+	// 	console.log(result);
+	// });
+
+	//test
+	console.log("關鍵字"+keyword);
 })
 
 //這個可能要加Detail_id
 app.get('/Product/Details',function(req, res){
-    
+	//取得查詢的detail_id
+	var detail_id = req.body.id;
+
+	//test
+	console.log('查詢'+detail_id);
 })
 
 //要加上所有產品Database需要的Column
 app.post('/Product',function(req, res){
-    
+	
 })
 
 var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log("Example app listening at http://%s:%s", host, port)
 })
