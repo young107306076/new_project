8357b5afc6f8d018e780af60fc5f60ff4494cb49
Young <44830681+young107306076@users.noreply.github.com>
2022-04-14 17:33:42 +0800

    adjust express console

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.get('/api/product/search',function(req, res){//這則是另外一種，用bo
 	// });
 
 	//test
-	console.log(req.body);
+	console.log(req.query);
 })
 
 //這個可能要加Detail_id (since it is one single product and above of them are a bunch of products)
