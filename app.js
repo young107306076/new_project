8ea600429fb36a3e96e1ca391ff29030dd6c0601
Young <44830681+young107306076@users.noreply.github.com>
2022-04-24 00:07:14 +0800

    unit test with signup

--- app.js
+++ app.js
@@ -292,7 +292,7 @@ app.post('/api/v1/users/signup',(req, res)=>{
 	try{
 
 		//store in database (user table)
-		query = "insert into user values ('"+user_id+"','"+user_email+"','"+hashed_psw+"','True','2022-04-20','2022-04-20)";
+		query = "insert into user values ('"+user_id+"','"+user_email+"','"+hashed_psw+"','True','2022-04-20','2022-04-20')";
 		query2 = "insert into user_detail ('user_id','name','gender','phone','address','photo_url') "+
 				"values ('"+user_id+"','"+user_name+"','"+user_gender+"','"+user_phone+"','"+user_address+"','"+user_photo_url+"')";
 		//query3 = "insert into user_login values ('"+product_color_id+"','"+product_id+"','"+product_color+"')";
