752ea317744b338b23076c73463df09e6affd9a6
Young <44830681+young107306076@users.noreply.github.com>
2022-04-24 16:05:18 +0800

    unit test with request api to tappay

--- app.js
+++ app.js
@@ -360,22 +360,23 @@ app.post('/api/v1/user/test',async function(req, res){
 		`-H "Content-Type: application/json" `+
 		`-H "x-api-key: ${process.env.partner_key} " `+
 		`-d '{
-			"prime":${prime},
 			"partnet_key":"${process.env.partner_key}",
+			"prime":${prime},
 			"merchant_id":${merchant_id},
 			"details":"TapPay Test",
 			"amount":100,
-			"cardholder":{"phone_number":"0955555555",
-						  "name":"Young",
-						  "email":"young30310@gmail.com",
-						  "zip_code":"12345",
-						  "address":"Taiwan",
-						  "national_id":"A123456789"
-						},
-			"remember":true
+			"cardholder":{
+				"phone_number":"0955555555",
+				"name":"Young",
+				"email":"young30310@gmail.com",
+				"zip_code":"12345",
+				"address":"台北市天龍區芝麻街1號1樓",
+				"national_id":"A123456789"
+			}
 		}' `);
 
 	console.log(stdout)
+	console.log(stderr)
 })
 
 //log in page
