f09c15a95b75ef2e67b2dddfe74ec82fd6522bc9
Young <44830681+young107306076@users.noreply.github.com>
2022-04-24 19:40:26 +0800

    unit test with request api to tappay

--- app.js
+++ app.js
@@ -605,7 +605,7 @@ app.post('/api/v1/order/checkout',async function(req,res){
 		//若銀行端顯示付款成功，則將該筆訂單的付款欄位改為True
 		//並新增一筆付款成功的訂單 (這有必要???)
 		let outcome=xhr.responseText;
-		console.log(outcome[0])
+		console.log(outcome[0].status)
 		if(outcome=="0"){
 			console.log("success")
 			res.send({
